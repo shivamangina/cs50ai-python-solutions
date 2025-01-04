eabbda0e110fa2bfae926ebef5bac88875ca48f8
Shiva Kumar Mangina <shiva2nani.mangina@gmail.com>
2025-01-04 23:27:43 +0530

    fix: Add Traffic

--- traffic/traffic.py
+++ traffic/traffic.py
@@ -34,10 +34,10 @@ def main():
     # Fit model on training data
     model.fit(x_train, y_train, epochs=EPOCHS)
 
-    # Evaluate neural network performance
+    # # Evaluate neural network performance
     model.evaluate(x_test,  y_test, verbose=2)
 
-    # Save model to file
+    # # Save model to file
     if len(sys.argv) == 3:
         filename = sys.argv[2]
         model.save(filename)
@@ -58,7 +58,30 @@ def load_data(data_dir):
     be a list of integer labels, representing the categories for each of the
     corresponding `images`.
     """
-    raise NotImplementedError
+    
+    images = []
+    labels = []
+    
+    # Iterate over each category directory
+    for category in range(NUM_CATEGORIES):
+        category_dir = os.path.join(data_dir, str(category))
+        
+        # Iterate over each image file in the category directory
+        for filename in os.listdir(category_dir):
+            img_path = os.path.join(category_dir, filename)
+            
+            # Read the image
+            img = cv2.imread(img_path)
+            
+            # Resize the image to the desired dimensions
+            img = cv2.resize(img, (IMG_WIDTH, IMG_HEIGHT))
+            
+            # Append the image and label to the lists
+            images.append(img)
+            labels.append(category)
+    
+    return (images, labels)
+    
 
 
 def get_model():
@@ -67,7 +90,44 @@ def get_model():
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
-    raise NotImplementedError
+    model = tf.keras.models.Sequential([
+        # Convolutional layer. Learn 32 filters using a 3x3 kernel
+        tf.keras.layers.Conv2D(
+            32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+        ),
+
+        # Max-pooling layer, using 2x2 pool size
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+        tf.keras.layers.Conv2D(
+            32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+        ),
+
+        # Max-pooling layer, using 2x2 pool size
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+        # Flatten units
+        tf.keras.layers.Flatten(),
+
+        # Add a hidden layer with dropout
+        tf.keras.layers.Dense(128, activation="relu"),
+        tf.keras.layers.Dropout(0.5),
+
+        # Add an output layer with output units for all 10 digits
+        tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
+    ])
+
+    model.compile(
+        optimizer="adam",
+        loss="categorical_crossentropy",
+        metrics=["accuracy"]
+    )
+
+    model.summary()
+    
+    return model
+    
+    
 
 
 if __name__ == "__main__":
